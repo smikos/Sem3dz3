286f1367ce05020acfb282c9c526249197123dc1
Konstantin Smirnov <sasharugby@yandex.ru>
2023-06-20 22:33:36 +0300

    all good

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int ReadInt(string message)
+{
+    System.Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int number = ReadInt("Введите целое положительное число >");
+int x =1;
+
+while( x<=number)
+{
+    int res =x*x*x;
+    x++;
+    Console.WriteLine(res);
+}
